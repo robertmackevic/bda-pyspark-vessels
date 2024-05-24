72f51a68ae60628e65e3c71b6748dfa4237b6597
Tomas Stoskus <tstoskus@scope-international.com>
2024-05-24 19:40:50 +0300

    Add young og added police to the data 🤦‍♂️

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from folium import Map, PolyLine
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, IntegerType
 from pyspark.sql.window import Window
-
+from matplotlib import pyplot as plt
 from src.distance import haversine_distance
 from src.paths import DATASET_CSV, DATA_DIR
 
@@ -36,14 +36,32 @@ def run() -> None:
     windowSpec = Window.partitionBy("MMSI").orderBy("timestamp")
     df = df.withColumn("prev_latitude", f.lag("latitude").over(windowSpec))
     df = df.withColumn("prev_longitude", f.lag("longitude").over(windowSpec))
-    df = df.dropna(subset=["prev_latitude", "prev_longitude"])
+    df = df.withColumn("prev_time", f.lag("timestamp").over(windowSpec))
+    df = df.dropna(subset=["prev_latitude", "prev_longitude", "prev_time"])
 
     df = df.filter((df["prev_latitude"] != df["latitude"]) | (df["prev_longitude"] != df["longitude"]))
 
     df = df.withColumn("distance", haversine_distance(
         f.col("prev_latitude"), f.col("prev_longitude"), f.col("latitude"), f.col("longitude")
     ))
+    df = df.withColumn("time_diff", f.col("timestamp").cast("long") - f.col("prev_time").cast("long"))
+    df = df.withColumn("speed", f.col("distance") / f.col("time_diff") * 3600)
+    
+    df = df.filter(df["speed"] < 50)
 
+    # sample_df = df.sample(False, 100000 / df.count(), seed=42)
+    # samples = [i["speed"] for i in sample_df.select("speed").collect() if i["speed"] is not None]
+    # plt.figure(figsize=(10, 6))
+    # plt.hist(samples, bins=500, color='skyblue', edgecolor='black')
+    # plt.xlabel('Speed (km/h)')
+    # plt.ylabel('Frequency')
+    # plt.title('Speed Distribution')
+    # plt.grid(True)
+    # plt.show()
+
+
+
+    
     grouped_df = df.groupBy("MMSI").agg(f.sum("distance").alias("total_distance"))
     top_5_mmsi = grouped_df.orderBy(grouped_df["total_distance"].desc()).limit(5)
     print("Top 5 Distance Traveled by Vessel (MMSI):")
